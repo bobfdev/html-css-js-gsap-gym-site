a630202d8119ab1ac82dd3f8b47d7575c95ddf5f
Bob F <bobfdev@gmail.com>
2022-01-19 16:27:45 -0600

    completed files

--- script.js
+++ script.js
@@ -16,3 +16,64 @@ const mobileMenu = () => {
 }
 
 menu.addEventListener('click', mobileMenu)
+
+// GSAP Animations
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.from('.animate-hero', {
+    duration: 0.6,
+    opacity: 0,
+    y: -150,
+    stagger: 0.3,
+})
+
+gsap.from('.animate-services', {
+    scrollTrigger: '.animate-services',
+    duration: 0.6,
+    opacity: 0,
+    x: -150,
+    stagger: 0.2,
+})
+
+gsap.from('.animate-img', {
+    // scrollTrigger: '.animate-services',
+    duration: 0.9,
+    opacity: 0,
+    y: -200,
+})
+
+gsap.from('.animate-membership', {
+    scrollTrigger: '.animate-membership',
+    duration: 0.6,
+    opacity: 0,
+    x: -150,
+    stagger: 0.3,
+    delay: 0.5, 
+})
+
+gsap.from('.animate-card', {
+    scrollTrigger: '.animate-card',
+    duration: 0.6,
+    opacity: 0,
+    y: -150,
+    stagger: 0.3,
+    delay: 0.2, 
+})
+
+gsap.from('.animate-team', {
+    scrollTrigger: '.animate-team',
+    duration: 0.6,
+    opacity: 0,
+    y: -150,
+    stagger: 0.3,
+    delay: 0.2, 
+})
+
+gsap.from('.animate-email', {
+    scrollTrigger: '.animate-email',
+    duration: 0.6,
+    opacity: 0,
+    x: -150,
+    stagger: 0.3,
+    delay: 0.4, 
+})
